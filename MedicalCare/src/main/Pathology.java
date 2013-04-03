6813d66348bc4f121d14fcedc8ee390cd4e4bbbc
Katia <katia.chiron@gmail.com>
2013-04-03 20:33:55 +0200

    Fix some bugs

---
+++ MedicalCare/src/main/Pathology.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ *
+ * @author Aurelie
+ */
+public enum Pathology {
+	/**
+     *
+     */
+    Diabetes_type_1, Diabetes_type_2 , Hypertension , viral_hepatitis , cancer , myocardial_infarction, Arthrite_rhumatoïde, Mucoviscidose, maladie_auto_immune, Maladie_de_Parkinson, Maladie_Alzheimer ; 
+
+}
--- MedicalCare/src/persons/Doctor.java
+++ MedicalCare/src/persons/Doctor.java
@@ -1,16 +1,49 @@
 package persons;
 
 public class Doctor extends Actor {
-
+                      private String login;
+	private String password;
 	/**
 	 * 
 	 * @param id
 	 * @param firstName
 	 * @param lastName
-	 * @param password
+                       * @param password
+                        * @param login  
 	 */
-	public Doctor(String id, String firstName, String lastName, String password) {
-		throw new UnsupportedOperationException();
+	public Doctor(String id, String firstName, String lastName, String password, String login) {
+		super(id, firstName, lastName);
+                this.password=password;
+                this.login=login;
 	}
+        
+                /**
+     *
+     * @return
+     */
+    public String getLogin(){
+            return this.login;
+        }
+        /**
+     *
+     * @param login
+     */
+    public void setLogin(String login){
+            this.login=login;
+        }
+        /**
+     *
+     * @return
+     */
+    public String getPassword(){
+            return this.password;
+        }
+        /**
+     *
+     * @param password
+     */
+    public void setPassword(String password){
+            this.password=password;
+        }
 
 }
