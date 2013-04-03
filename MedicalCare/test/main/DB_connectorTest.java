73989b73d3f6862b391f00d9ce31f9b468422163
Katia <katia.chiron@gmail.com>
2013-04-03 20:39:10 +0200

    Fix the last bug !

--- MedicalCare/test/main/DB_connectorTest.java
+++ MedicalCare/test/main/DB_connectorTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -50,7 +49,7 @@ public class DB_connectorTest {
     public void testAddPatient() {
         System.out.println("addPatient");
         Patient p = null;
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         boolean expResult = false;
         boolean result = instance.addPatient(p);
         assertEquals(expResult, result);
@@ -65,7 +64,7 @@ public class DB_connectorTest {
     public void testAddBloodTest() {
         System.out.println("addBloodTest");
         BloodTest bt = null;
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         boolean expResult = false;
         boolean result = instance.addBloodTest(bt);
         assertEquals(expResult, result);
@@ -80,305 +79,7 @@ public class DB_connectorTest {
     public void testAddEEG() {
         System.out.println("addEEG");
         EEG electro = null;
-        DB_connector instance = new DB_connector();
-        boolean expResult = false;
-        boolean result = instance.addEEG(electro);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of addEffortTest method, of class DB_connector.
-     */
-    @Test
-    public void testAddEffortTest() {
-        System.out.println("addEffortTest");
-        EffortTest effort = null;
-        DB_connector instance = new DB_connector();
-        boolean expResult = false;
-        boolean result = instance.addEffortTest(effort);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of addDoctor method, of class DB_connector.
-     */
-    @Test
-    public void testAddDoctor() {
-        System.out.println("addDoctor");
-        DB_connector instance = new DB_connector();
-        instance.addDoctor();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of addCRA method, of class DB_connector.
-     */
-    @Test
-    public void testAddCRA() {
-        System.out.println("addCRA");
-        DB_connector instance = new DB_connector();
-        instance.addCRA();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getUncheckedInfo method, of class DB_connector.
-     */
-    @Test
-    public void testGetUncheckedInfo() {
-        System.out.println("getUncheckedInfo");
-        DB_connector instance = new DB_connector();
-        DailyTest[] expResult = null;
-        DailyTest[] result = instance.getUncheckedInfo();
-        assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of userSelection method, of class DB_connector.
-     */
-    @Test
-    public void testUserSelection() {
-        System.out.println("userSelection");
-        String login = "";
-        String password = "";
-        DB_connector instance = new DB_connector();
-        instance.userSelection(login, password);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of delPatient method, of class DB_connector.
-     */
-    @Test
-    public void testDelPatient() {
-        System.out.println("delPatient");
-        String id = "";
-        DB_connector instance = new DB_connector();
-        instance.delPatient(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getDoctorConsultations method, of class DB_connector.
-     */
-    @Test
-    public void testGetDoctorConsultations() {
-        System.out.println("getDoctorConsultations");
-        int idDoctor = 0;
-        DB_connector instance = new DB_connector();
-        ArrayList expResult = null;
-        ArrayList result = instance.getDoctorConsultations(idDoctor);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getPatient method, of class DB_connector.
-     */
-    @Test
-    public void testGetPatient() {
-        System.out.println("getPatient");
-        String id = "";
-        DB_connector instance = new DB_connector();
-        instance.getPatient(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getListPatient method, of class DB_connector.
-     */
-    @Test
-    public void testGetListPatient() {
-        System.out.println("getListPatient");
-        DB_connector instance = new DB_connector();
-        ArrayList expResult = null;
-        ArrayList result = instance.getListPatient();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getDailyTests method, of class DB_connector.
-     */
-    @Test
-    public void testGetDailyTests() {
-        System.out.println("getDailyTests");
-        String idPatient = "";
-        String day = "";
-        DB_connector instance = new DB_connector();
-        instance.getDailyTests(idPatient, day);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of updateDailyTest method, of class DB_connector.
-     */
-    @Test
-    public void testUpdateDailyTest() {
-        System.out.println("updateDailyTest");
-        int idTest = 0;
-        DB_connector instance = new DB_connector();
-        instance.updateDailyTest(idTest);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of addSubGroup method, of class DB_connector.
-     */
-    @Test
-    public void testAddSubGroup() {
-        System.out.println("addSubGroup");
-        Group sg = null;
-        DB_connector instance = new DB_connector();
-        boolean expResult = false;
-        boolean result = instance.addSubGroup(sg);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setSubGroup method, of class DB_connector.
-     */
-    @Test
-    public void testSetSubGroup() {
-        System.out.println("setSubGroup");
-        Patient p = null;
-        DB_connector instance = new DB_connector();
-        boolean expResult = false;
-        boolean result = instance.setSubGroup(p);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of addPatientToGroup method, of class DB_connector.
-     */
-    @Test
-    public void testAddPatientToGroup() {
-        System.out.println("addPatientToGroup");
-        Patient p = null;
-        Group g = null;
-        DB_connector instance = new DB_connector();
-        instance.addPatientToGroup(p, g);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getInfoSubGroup method, of class DB_connector.
-     */
-    @Test
-    public void testGetInfoSubGroup() {
-        System.out.println("getInfoSubGroup");
-        String id = "";
-        DB_connector instance = new DB_connector();
-        Group expResult = null;
-        Group result = instance.getInfoSubGroup(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-}
-=======
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package main;
-
-import java.util.ArrayList;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import persons.Patient;
-import tests.BloodTest;
-import tests.DailyTest;
-import tests.EEG;
-import tests.EffortTest;
-
-/**
- *
- * @author Cogotch
- */
-public class DB_connectorTest {
-    
-    public DB_connectorTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of addPatient method, of class DB_connector.
-     */
-    @Test
-    public void testAddPatient() {
-        System.out.println("addPatient");
-        Patient p = null;
-        DB_connector instance = new DB_connector();
-        boolean expResult = false;
-        boolean result = instance.addPatient(p);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of addBloodTest method, of class DB_connector.
-     */
-    @Test
-    public void testAddBloodTest() {
-        System.out.println("addBloodTest");
-        BloodTest bt = null;
-        DB_connector instance = new DB_connector();
-        boolean expResult = false;
-        boolean result = instance.addBloodTest(bt);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of addEEG method, of class DB_connector.
-     */
-    @Test
-    public void testAddEEG() {
-        System.out.println("addEEG");
-        EEG electro = null;
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         boolean expResult = false;
         boolean result = instance.addEEG(electro);
         assertEquals(expResult, result);
@@ -392,7 +93,7 @@ public class DB_connectorTest {
     public void testAddEffortTest() {
         System.out.println("addEffortTest");
         EffortTest effort = null;
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         boolean expResult = false;
         boolean result = instance.addEffortTest(effort);
         assertEquals(expResult, result);
@@ -405,7 +106,7 @@ public class DB_connectorTest {
     @Test
     public void testAddDoctor() {
         System.out.println("addDoctor");
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         instance.addDoctor();
 
     }
@@ -416,7 +117,7 @@ public class DB_connectorTest {
     @Test
     public void testAddCRA() {
         System.out.println("addCRA");
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         instance.addCRA();
 
     }
@@ -427,7 +128,7 @@ public class DB_connectorTest {
     @Test
     public void testGetUncheckedInfo() {
         System.out.println("getUncheckedInfo");
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         DailyTest[] expResult = null;
         DailyTest[] result = instance.getUncheckedInfo();
         assertArrayEquals(expResult, result);
@@ -442,7 +143,7 @@ public class DB_connectorTest {
         System.out.println("userSelection");
         String login = "";
         String password = "";
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         instance.userSelection(login, password);
 
     }
@@ -454,7 +155,7 @@ public class DB_connectorTest {
     public void testDelPatient() {
         System.out.println("delPatient");
         String id = "";
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         instance.delPatient(id);
 
     }
@@ -466,7 +167,7 @@ public class DB_connectorTest {
     public void testGetDoctorConsultations() {
         System.out.println("getDoctorConsultations");
         int idDoctor = 0;
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         ArrayList expResult = null;
         ArrayList result = instance.getDoctorConsultations(idDoctor);
         assertEquals(expResult, result);
@@ -480,8 +181,8 @@ public class DB_connectorTest {
     public void testGetPatient() {
         System.out.println("getPatient");
         String id = "";
-        DB_connector instance = new DB_connector();
-        instance.getPatient(id);
+        DB_connector instance =null;
+        //DB_connector.getPatient(id);
 
     }
 
@@ -491,7 +192,7 @@ public class DB_connectorTest {
     @Test
     public void testGetListPatient() {
         System.out.println("getListPatient");
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         ArrayList expResult = null;
         ArrayList result = instance.getListPatient();
         assertEquals(expResult, result);
@@ -506,7 +207,7 @@ public class DB_connectorTest {
         System.out.println("getDailyTests");
         String idPatient = "";
         String day = "";
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         instance.getDailyTests(idPatient, day);
 
     }
@@ -518,7 +219,7 @@ public class DB_connectorTest {
     public void testUpdateDailyTest() {
         System.out.println("updateDailyTest");
         int idTest = 0;
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         instance.updateDailyTest(idTest);
 
     }
@@ -530,7 +231,7 @@ public class DB_connectorTest {
     public void testAddSubGroup() {
         System.out.println("addSubGroup");
         Group sg = null;
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         boolean expResult = false;
         boolean result = instance.addSubGroup(sg);
         assertEquals(expResult, result);
@@ -544,7 +245,7 @@ public class DB_connectorTest {
     public void testSetSubGroup() {
         System.out.println("setSubGroup");
         Patient p = null;
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         boolean expResult = false;
         boolean result = instance.setSubGroup(p);
         assertEquals(expResult, result);
@@ -559,7 +260,7 @@ public class DB_connectorTest {
         System.out.println("addPatientToGroup");
         Patient p = null;
         Group g = null;
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         instance.addPatientToGroup(p, g);
 
     }
@@ -571,11 +272,10 @@ public class DB_connectorTest {
     public void testGetInfoSubGroup() {
         System.out.println("getInfoSubGroup");
         String id = "";
-        DB_connector instance = new DB_connector();
+        DB_connector instance =null;
         Group expResult = null;
         Group result = instance.getInfoSubGroup(id);
         assertEquals(expResult, result);
 
     }
 }
->>>>>>> 2b920e79b2d4f6488e9d387b426e5dfce10817bb
