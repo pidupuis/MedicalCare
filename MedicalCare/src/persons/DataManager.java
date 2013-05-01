acaacb5f7db608fa5c8086b116b6ee5432a0a669
Cogotch <Cogotch@172.16.127.67>
2013-05-01 21:14:55 +0200

    MaJ des classes group et dm by lolo

--- MedicalCare/src/persons/DataManager.java
+++ MedicalCare/src/persons/DataManager.java
@@ -6,7 +6,9 @@ import main.*;
 
 
 /**
- *
+ * Class that implements attributes of the datamanager and the assignement of the patients
+ * to groups. 
+ * 
  * @author Cogotch
  */
 public class DataManager extends Actor {
@@ -17,29 +19,28 @@ public class DataManager extends Actor {
 
     /**
      *
-     * @param id
-     * @param firstName
-     * @param lastName
-     * @param password
-     * @param login
+     * @param firstName is the first name of the DataManager
+     * @param lastName is the lastname of the datamanager
+     * @param password is the password for connection for the HcI
+     * @param login is the login for connection for the HcI
      */
-    public DataManager(String id, String firstName, String lastName, String password, String login) {
-        super(id, firstName, lastName);
-        this.password = password;
-        this.login = login;
+    public DataManager(String firstName, String lastName) throws Exception {
+        super(firstName, lastName);
+        //this.password = password;
+        //this.login = login;
         this.assignment = false;
     }
 
     /**
-     *
-     * @return
+     * getter of the login of the datamanager
+     * @return login 
      */
     public String getLogin() {
         return this.login;
     }
 
     /**
-     *
+     * setter of the login of the datamanager
      * @param login
      */
     public void setLogin(String login) {
@@ -47,7 +48,7 @@ public class DataManager extends Actor {
     }
 
     /**
-     *
+     * getter of the password of the datamanager
      * @return
      */
     public String getPassword() {
@@ -55,49 +56,79 @@ public class DataManager extends Actor {
     }
 
     /**
-     *
+     * setter of the password of the datamanager
      * @param password
      */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * setter of the statute of the assignement
+     * @param b
+     */
     public void setAssignment(boolean b) {
-        this.assignment = b;
+        if (b==true) {
+            if (getAssignment()==false) {
+                this.assignment = b;
+            }
+            else {
+                System.out.println("Assignement already done! You can't do it again!");
+            }
+        }
+        else{
+            System.out.println("You can't do it!");
+            }
     }
 
+    /**
+     * getter of the statute of the assignement 
+     * @return
+     */
     public boolean getAssignment() {
         return (this.assignment);
     }
 
+    /**
+     * Class that do automaticaly the assignement of the patients in groups 
+     * 
+     * @throws Exception
+     */
     public void assignment() throws Exception {
-        if (this.assignment== false) {
+        
+        if (this.assignment== false) { // if the assignement is on false we can do it
+            /* declaration zone */
             int i =0;
             int cptGlob,sizeP,cpt55;
             int age;
             int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-
             ArrayList<Patient> listPatient = new ArrayList();
             listPatient = DB_connector.getListPatient();
+            
+            /* list of patients is shuffleling, it's right for the rondomization */
             Collections.shuffle(listPatient);
-            sizeP = listPatient.size();
-            cptGlob = 1;
-            cpt55 = 1;
-            Group lG= new Group();
-            for (i=0; i<sizeP;i++){
-                age=currentYear-listPatient.get(i).getBirthDate().get(Calendar.YEAR);
-                if (age>=55){
-                    if (cpt55%2!=0) {
-                        lG.getGroupe("PP1").add(listPatient.get(i));
+            
+            /* initialization of counters */
+            sizeP = listPatient.size(); //size of the list of patients
+            cptGlob = 1; //counter using for the attribution of patients in groups
+            cpt55 = 1;   //counter using only for the attribution of the patients more of 55 years old
+            Group lG= new Group(); 
+            
+            /* starting of the attribution of patients to groups */
+            for (i=0; i<sizeP;i++){  
+                age=currentYear-listPatient.get(i).getBirthDate().get(Calendar.YEAR);//we need age of patients for the assignement
+                if (age>=55){ //if patient have more of 55 years old
+                    if (cpt55%2!=0) { //if cpt55 is pair
+                        lG.getGroupe("PP1").add(listPatient.get(i)); //we add the patient to the first group that takes placebo only
                         cpt55+=1;
                     }
                     else {
                         lG.getGroupe("PP2").add(listPatient.get(i));
-                        cpt55+=1; //je remet à zéro pour que le prochain assignement se fasse 
+                        cpt55+=1;
                     }
                 }
-                else {
-                    if (cpt55 <=(lG.getGroupe("TV").size())) {
+                else { //if patient is less of 55 years old we can attribute it for a group
+                    if (cpt55 <=(lG.getGroupe("TV").size())) { // this condition is use to bring homogenous groups
                         switch (cptGlob) { 
                             case 1: 
                                 lG.getGroupe("PP1").add(listPatient.get(i));
@@ -175,18 +206,6 @@ public class DataManager extends Actor {
                     }
                 }
             }
-        System.out.println("Cpt55 : "+cpt55);
-        System.out.println("Cptglob : "+cptGlob);
-        System.out.println("PP1 : "+lG.getGroupe("PP1").size());
-        System.out.println("PP2 : "+lG.getGroupe("PP2").size());
-        System.out.println("TP : "+lG.getGroupe("TP").size());
-        System.out.println("TV : "+lG.getGroupe("TV").size());
-        System.out.println("VP : "+lG.getGroupe("TValtPP").size());
-        System.out.println("TPaltPP : "+lG.getGroupe("TPaltPP").size());
-        System.out.println("VPalPP : "+lG.getGroupe("VPaltPP").size());
-        System.out.println("TValtPP : "+lG.getGroupe("TValtPP").size());
-        
-        
         } 
         else {
             System.out.println("L'assignement a déjà été effectué. Vous ne pouvez pas le relancer");
