dd837fbb9ab323e81e5aed3766f0c81f81084113
joron <aureliejoron@gmail.com>
2013-05-01 19:17:11 +0200

    I have completed the pathology class

--- MedicalCare/src/main/Pathology.java
+++ MedicalCare/src/main/Pathology.java
@@ -5,9 +5,32 @@ package main;
  * @author Aurelie
  */
 public enum Pathology {
-	/**
-     *
-     */
-    Diabetes_type_1, Diabetes_type_2 , Hypertension , viral_hepatitis , cancer , myocardial_infarction, Arthrite_rhumatoïde, Mucoviscidose, maladie_auto_immune, Maladie_de_Parkinson, Maladie_Alzheimer ; 
 
+    Diabetes_type_1 ("Diabète de type 1"),
+    Diabetes_type_2 ("Diabète de type 2"),
+    Hypertension ("Hypertension"), 
+    viral_hepatitis ("Hepathite viral"),
+    cancer ("Cancer"),
+    myocardial_infarction( "infarctus du myocarde"),
+    Arthrite_rhumatoïde ( "Arthrite rhumatoide"),
+    Mucoviscidose ("Mucoviscidose"),
+    maladie_auto_immune ("maladie auto-immune"),
+    Maladie_de_Parkinson ("Maladie de Parkinson"),
+    Maladie_Alzheimer ("Maladie d'Alheimer"); 
+
+    private String description;
+    private Pathology (String description){
+        this.description = description;
+    }
+    public static void affichePatho (){
+        Pathology [] patho = Pathology.values();
+        System.out.println("\n Liste des pathologies:");
+        for (int i=0; i< patho.length; i++){
+            if (patho[i] == Diabetes_type_1){
+                System.out.println(patho[i].description);
+            }
+            else
+                System.out.println(patho[i].description);
+        }
+    }
 }
