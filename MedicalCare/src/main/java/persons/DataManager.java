b53247fbccc6f99948b572afe3a7ebd20c0c7fd6
keuv <keuv@keuv-305U1A>
2013-05-15 12:07:36 +0200

    Revert "Add javadoc & refactoring"
    This reverts commit 8fa1edc39380fe7bbca6404782acc6e511fe5ee2.

--- MedicalCare/src/main/java/persons/DataManager.java
+++ MedicalCare/src/main/java/persons/DataManager.java
@@ -1,27 +1,20 @@
 package persons;
-
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import main.*;
 
+
 /**
- * Class that implements attributes of the datamanager and the assignement of
- * the patients to groups.
- *
+ * Class that implements attributes of the datamanager and the assignement of the patients
+ * to groups. 
+ * 
  * @author Cogotch
  */
 public class DataManager extends Actor {
 
-    /**
-     * DataManager password
-     */
     private String password;
-    /**
-     * DataManager login
-     */
     private String login;
-    
     private boolean assignment;
 
     /**
@@ -33,15 +26,14 @@ public class DataManager extends Actor {
      */
     public DataManager(String firstName, String lastName) throws Exception {
         super(firstName, lastName);
-        this.generateLogin();
+        this.generateLogin(firstName, lastName);
         this.password = this.login;
         this.assignment = false;
     }
 
     /**
      * getter of the login of the datamanager
-     *
-     * @return login
+     * @return login 
      */
     public String getLogin() {
         return this.login;
@@ -49,7 +41,6 @@ public class DataManager extends Actor {
 
     /**
      * setter of the login of the datamanager
-     *
      * @param login
      */
     public void setLogin(String login) {
@@ -58,7 +49,6 @@ public class DataManager extends Actor {
 
     /**
      * getter of the password of the datamanager
-     *
      * @return
      */
     public String getPassword() {
@@ -67,7 +57,6 @@ public class DataManager extends Actor {
 
     /**
      * setter of the password of the datamanager
-     *
      * @param password
      */
     public void setPassword(String password) {
@@ -76,25 +65,24 @@ public class DataManager extends Actor {
 
     /**
      * setter of the statute of the assignement
-     *
      * @param b
      */
     public void setAssignment(boolean b) {
-        if (b == true) {
-            this.assignment = b;
-            if (getAssignment() == false) {
+        if (b==true) {
+            if (getAssignment()==false) {
                 this.assignment = b;
-            } else {
+            }
+            else {
                 System.out.println("Assignement already done! You can't do it again!");
             }
-        } else {
-            System.out.println("You can't do it!");
         }
+        else{
+            System.out.println("You can't do it!");
+            }
     }
 
     /**
-     * getter of the statute of the assignement
-     *
+     * getter of the statute of the assignement 
      * @return
      */
     public boolean getAssignment() {
@@ -102,147 +90,145 @@ public class DataManager extends Actor {
     }
 
     /**
-     * Class that do automaticaly the assignement of the patients in groups
-     *
+     * Class that do automaticaly the assignement of the patients in groups 
+     * 
      * @throws Exception
      */
     public void assignment() throws Exception {
         DB_connector dbc = DB_connector.getInstance();
-        if (this.assignment == false) { // if the assignement is on false we can do it
+        if (this.assignment== false) { // if the assignement is on false we can do it
             /* declaration zone */
-            int i = 0;
-            int cptGlob, sizeP, cpt55;
+            int i =0;
+            int cptGlob,sizeP,cpt55;
             int age;
             int currentYear = Calendar.getInstance().get(Calendar.YEAR);
             ArrayList<Patient> listPatient = new ArrayList<Patient>();
             listPatient = dbc.getListPatient();
-
+            
             /* list of patients is shuffleling, it's right for the rondomization */
             Collections.shuffle(listPatient);
-
+            
             /* initialization of counters */
             sizeP = listPatient.size(); //size of the list of patients
             cptGlob = 1; //counter using for the attribution of patients in groups
             cpt55 = 1;   //counter using only for the attribution of the patients more of 55 years old
-            Group lG = new Group();
-
+            Group lG= new Group(); 
+            
             /* starting of the attribution of patients to groups */
-            for (i = 0; i < sizeP; i++) {
-                age = currentYear - listPatient.get(i).getBirthDate().get(Calendar.YEAR);//we need age of patients for the assignement
-                if (age >= 55) { //if patient have more of 55 years old
-                    if (cpt55 % 2 != 0) { //if cpt55 is pair
+            for (i=0; i<sizeP;i++){  
+                age=currentYear-listPatient.get(i).getBirthDate().get(Calendar.YEAR);//we need age of patients for the assignement
+                if (age>=55){ //if patient have more of 55 years old
+                    if (cpt55%2!=0) { //if cpt55 is pair
                         lG.getGroupe("PP1").add(listPatient.get(i)); //we add the patient to the first group that takes placebo only
-                        cpt55 += 1;
-                    } else {
-                        lG.getGroupe("PP2").add(listPatient.get(i));
-                        cpt55 += 1;
+                        cpt55+=1;
                     }
-                } else { //if patient is less of 55 years old we can attribute it for a group
-                    if (lG.getGroupe("PP1").size() <= (lG.getGroupe("TV").size())) { // this condition is use to bring homogenous groups
-                        switch (cptGlob) {
-                            case 1:
+                    else {
+                        lG.getGroupe("PP2").add(listPatient.get(i));
+                        cpt55+=1;
+                    }
+                }
+                else { //if patient is less of 55 years old we can attribute it for a group
+                    if (lG.getGroupe("PP1").size()<=(lG.getGroupe("TV").size())){ // this condition is use to bring homogenous groups
+                        switch (cptGlob) { 
+                            case 1: 
                                 lG.getGroupe("PP1").add(listPatient.get(i));
-                                cptGlob += 1;
-                                break;
-                            case 2:
-                                lG.getGroupe("PP2").add(listPatient.get(i));
-                                cptGlob += 1;
-                                break;
-                            case 3:
+                                cptGlob+=1;
+                                break; 
+                            case 2: 
+                                lG.getGroupe("PP2").add(listPatient.get(i)); 
+                                cptGlob+=1;
+                                break; 
+                            case 3: 
                                 lG.getGroupe("TV").add(listPatient.get(i));
-                                cptGlob += 1;
-                                break;
-                            case 4:
-                                lG.getGroupe("TValtPP").add(listPatient.get(i));
-                                cptGlob += 1;
-                                break;
-                            case 5:
+                                cptGlob+=1;
+                                break; 
+                            case 4: 
+                                lG.getGroupe("TValtPP").add(listPatient.get(i)); 
+                                cptGlob+=1;
+                                break; 
+                            case 5: 
                                 lG.getGroupe("TP").add(listPatient.get(i));
-                                cptGlob += 1;
-                                break;
-                            case 6:
-                                lG.getGroupe("TPaltPP").add(listPatient.get(i));
-                                cptGlob += 1;
-                                break;
-                            case 7:
+                                cptGlob+=1;
+                                break; 
+                            case 6: 
+                                lG.getGroupe("TPaltPP").add(listPatient.get(i)); 
+                                cptGlob+=1;
+                                break; 
+                            case 7: 
                                 lG.getGroupe("VP").add(listPatient.get(i));
-                                cptGlob += 1;
-                                break;
-                            case 8:
-                                lG.getGroupe("VPaltPP").add(listPatient.get(i));
-                                cptGlob = 1;
-                                break;
-                            default:
-                                throw new Exception("Erreur dans la répartition des groupes -> Contacter l'admin");
+                                cptGlob+=1;
+                                break; 
+                            case 8: 
+                                lG.getGroupe("VPaltPP").add(listPatient.get(i)); 
+                                cptGlob=1;
+                                break; 
+                            default: 
+                                throw new Exception ("Erreur dans la répartition des groupes -> Contacter l'admin"); 
                         }
-                    } else {
-                        switch (cptGlob) {
-                            case 1:
+                    }
+                    else{
+                        switch (cptGlob) { 
+                            case 1: 
                                 lG.getGroupe("TV").add(listPatient.get(i));
-                                cptGlob += 1;
-                                break;
-                            case 2:
-                                lG.getGroupe("TValtPP").add(listPatient.get(i));
-                                cptGlob += 1;
-                                break;
-                            case 3:
+                                cptGlob+=1;
+                                break; 
+                            case 2: 
+                                lG.getGroupe("TValtPP").add(listPatient.get(i)); 
+                                cptGlob+=1;
+                                break; 
+                            case 3: 
                                 lG.getGroupe("TP").add(listPatient.get(i));
-                                cptGlob += 1;
-                                break;
-                            case 4:
-                                lG.getGroupe("TPaltPP").add(listPatient.get(i));
-                                cptGlob += 1;
-                                break;
-                            case 5:
+                                cptGlob+=1;
+                                break; 
+                            case 4: 
+                                lG.getGroupe("TPaltPP").add(listPatient.get(i)); 
+                                cptGlob+=1;
+                                break; 
+                            case 5: 
                                 lG.getGroupe("VP").add(listPatient.get(i));
-                                cptGlob += 1;
+                                cptGlob+=1;
+                                break; 
+                            case 6 :
+                                lG.getGroupe("VPaltPP").add(listPatient.get(i)); 
+                                cptGlob=1;
                                 break;
-                            case 6:
-                                lG.getGroupe("VPaltPP").add(listPatient.get(i));
-                                cptGlob = 1;
+                            case 7 :
+                                lG.getGroupe("TV").add(listPatient.get(i)); 
+                                cptGlob=2;
                                 break;
-                            case 7:
-                                lG.getGroupe("TV").add(listPatient.get(i));
-                                cptGlob = 2;
+                            case 8 :
+                                lG.getGroupe("TV").add(listPatient.get(i)); 
+                                cptGlob=2;
                                 break;
-                            case 8:
-                                lG.getGroupe("TV").add(listPatient.get(i));
-                                cptGlob = 2;
-                                break;
-                            default:
-                                throw new Exception("Erreur dans la répartition des groupes -> Contacter l'admin");
+                            default: 
+                                throw new Exception ("Erreur dans la répartition des groupes -> Contacter l'admin"); 
                         }
                     }
                 }
             }
-            System.out.println("Cpt55 : " + cpt55);
-            System.out.println("Cptglob : " + cptGlob);
-            System.out.println("sizeP : " + sizeP);
-            System.out.println("PP1 : " + lG.getGroupe("PP1").size());
-            System.out.println("PP2 : " + lG.getGroupe("PP2").size());
-            System.out.println("TP : " + lG.getGroupe("TP").size());
-            System.out.println("TV : " + lG.getGroupe("TV").size());
-            System.out.println("VP : " + lG.getGroupe("TValtPP").size());
-            System.out.println("TPaltPP : " + lG.getGroupe("TPaltPP").size());
-            System.out.println("VPalPP : " + lG.getGroupe("VPaltPP").size());
-            System.out.println("TValtPP : " + lG.getGroupe("TValtPP").size());
-        } else {
+        System.out.println("Cpt55 : "+cpt55);
+        System.out.println("Cptglob : "+cptGlob);
+        System.out.println("sizeP : "+sizeP);
+        System.out.println("PP1 : "+lG.getGroupe("PP1").size());
+        System.out.println("PP2 : "+lG.getGroupe("PP2").size());
+        System.out.println("TP : "+lG.getGroupe("TP").size());
+        System.out.println("TV : "+lG.getGroupe("TV").size());
+        System.out.println("VP : "+lG.getGroupe("TValtPP").size());
+        System.out.println("TPaltPP : "+lG.getGroupe("TPaltPP").size());
+        System.out.println("VPalPP : "+lG.getGroupe("VPaltPP").size());
+        System.out.println("TValtPP : "+lG.getGroupe("TValtPP").size());
+        } 
+        else {
             System.out.println("L'assignement a déjà été effectué. Vous ne pouvez pas le relancer");
         }
     }
-
-    /**
-     * Auto generate the CRA login.
-     *
-     * @throws Exception
-     */
-    private void generateLogin() throws Exception {
-        String log = super.getFirstName().substring(0, 1) + super.getLastName();
+    private void generateLogin(String firstName, String lastName) throws Exception {
+        String log = firstName.substring(0, 1) + lastName;
 
         DB_connector db = DB_connector.getInstance();
-
-        log = log.replace(" ", "");
-        log = log.replace("-", "");
+        
+        log = log.replace(" ","");
+        log = log.replace("-","");
         log = log.toLowerCase();
         this.login = log;
         boolean continueLoop = true;
@@ -250,9 +236,11 @@ public class DataManager extends Actor {
         while (continueLoop) {
             if (db.checkUser(this.login)) {
                 this.login = log + String.valueOf(i);
-            } else {
+            }
+            else {
                 continueLoop = false;
             }
         }
     }
+
 }
