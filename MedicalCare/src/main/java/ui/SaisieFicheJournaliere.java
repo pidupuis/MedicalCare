591574b874a484e8281a1d54bba0c1b48dfac082
Mathieu <Mathieu@172.16.124.39>
2013-05-15 18:09:43 +0200

    Create a good tree from an amazing developer choose to change the doctor class

--- MedicalCare/src/main/java/ui/SaisieFicheJournaliere.java
+++ MedicalCare/src/main/java/ui/SaisieFicheJournaliere.java
@@ -2,9 +2,14 @@ package ui;
 
 import javax.swing.JPanel;
 import net.miginfocom.swing.MigLayout;
+
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
+
+import persons.Actor;
+import persons.Doctor;
 /**
  * JPanel to insert new daily form
  * 
@@ -16,25 +21,51 @@ public class SaisieFicheJournaliere extends JPanel {
 	/**
 	 * Create the panel.
 	 */
-	public SaisieFicheJournaliere() {
+	public SaisieFicheJournaliere(Actor user) {
+		
 		//partie de gauche de mon panel
-		DefaultMutableTreeNode root = new DefaultMutableTreeNode("Root");
+		
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode((user.getLastName()+" "+user.getFirstName()).toString());
 		
 		DefaultMutableTreeNode child1 = new DefaultMutableTreeNode("Child 1");
 		root.add(child1);
 		DefaultMutableTreeNode child2 = new DefaultMutableTreeNode("Child 2");
 		root.add(child2);
+		
+		DefaultMutableTreeNode grandchild1 = new DefaultMutableTreeNode("grandchild 1");
+		child2.add(grandchild1);
+		DefaultMutableTreeNode grandchild2 = new DefaultMutableTreeNode("grandchild 2");
+		child2.add(grandchild2);
+		
 		JTree tree = new JTree(root);
 	
 		this.add(tree);
+		
 		//partie de droite
 		
 			// remplissage de la fiche journaliere
+		
 		
 			//Remplissage des analyses si besoin
 		
 			//fleches qui permettent de faire passer les patients
 		
 	}
+	
+	public static void main (String[] args) {
+		
+		JFrame plop = new JFrame();
+		Actor actor;
+		try {
+			actor = new Doctor("John","Doeuf","doc1");
+			plop.add(new SaisieFicheJournaliere(actor));
+			plop.setVisible(true);
+		} catch (Exception e) {
+		}
+
+		
+	}
 
 }
+
+
