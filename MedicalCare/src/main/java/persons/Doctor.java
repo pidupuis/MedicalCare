a633c88c5a7493c7ff08fe6e85ef76afd7e95b60
keuv <keuv@keuv-305U1A>
2013-05-15 11:42:09 +0200

    Revert "Add new constructor"
    This reverts commit 5cd30af213e9e462674784949a7ba84ba0f38fc9.

--- MedicalCare/src/main/java/persons/Doctor.java
+++ MedicalCare/src/main/java/persons/Doctor.java
@@ -29,17 +29,6 @@ public class Doctor extends Actor {
     }
 
     /**
-     * Create a new doctor. CRA is set to 'null'
-     * @param firstName
-     * @param lastName
-     * @throws Exception 
-     */
-    public Doctor(String firstName, String lastName) throws Exception {
-        this(firstName, lastName, null);
-    }
-
-    
-    /**
      *
      * @return String
      */
