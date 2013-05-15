409840fcf53d7ddc4402c58f19b1d83237d306c6
Mathieu <Mathieu@172.16.124.39>
2013-05-15 14:14:19 +0200

    nouveau panel

--- MedicalCare/src/main/java/ui/SaisieFicheJournaliere.java
+++ MedicalCare/src/main/java/ui/SaisieFicheJournaliere.java
@@ -1,13 +1,21 @@
 package ui;
 
 import javax.swing.JPanel;
-
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+/**
+ * JPanel to insert new daily form
+ * 
+ * @author Mathieu
+ *
+ */
 public class SaisieFicheJournaliere extends JPanel {
 
 	/**
 	 * Create the panel.
 	 */
 	public SaisieFicheJournaliere() {
+		setLayout(new MigLayout("", "[][]", "[][]"));
 
 	}
 
