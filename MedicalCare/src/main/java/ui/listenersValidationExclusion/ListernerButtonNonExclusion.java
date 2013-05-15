ce10d4a144ef557cf518eed4f6d3f07db6532b00
Piotr <pierre.cressant@gmail.com>
2013-05-15 18:08:32 +0200

    Added listener for validationExclusion

---
+++ MedicalCare/src/main/java/ui/listenersValidationExclusion/ListernerButtonNonExclusion.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui.listenersValidationExclusion;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+import main.Main;
+import persons.CRA;
+import persons.Patient;
+
+/**
+ *
+ * @author piotr
+ */
+public class ListernerButtonNonExclusion implements ActionListener {
+
+    private ValidePane interf;
+    private Patient pat;
+
+    public ListernerButtonNonExclusion(ValidePane interf, Patient pat) {
+        this.interf = interf;
+        this.pat = pat;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String why = JOptionPane.showInputDialog(interf, "Pourquoi ne pas exclure " + this.pat.getFirstName() + " " + this.pat.getLastName(), "Raison de la non exclusion");
+        ((CRA) Main.getUser()).remove(pat);
+
+    }
+}
