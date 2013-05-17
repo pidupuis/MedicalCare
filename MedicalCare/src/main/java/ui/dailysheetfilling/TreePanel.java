3ebcb580c78798ec5d9e014c9171d893c1dea64b
pidupuis <pmj.dupuis@gmail.com>
2013-05-17 10:26:08 +0200

    minor changes

--- MedicalCare/src/main/java/ui/dailysheetfilling/TreePanel.java
+++ MedicalCare/src/main/java/ui/dailysheetfilling/TreePanel.java
@@ -17,36 +17,36 @@ import persons.Patient;
 public class TreePanel extends JPanel {
 	
 	public TreePanel(Actor user) {
-		
+
 		// Subpanel to construct the tree
-				DefaultMutableTreeNode root = new DefaultMutableTreeNode((user.getLastName()+" "+user.getFirstName()).toString());
-				
-				ArrayList<Patient> myPatients = null;
-				
-				try {
-					myPatients = DB_connector.getInstance().getListPatientFromDoctor((Doctor) user);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				
-				DefaultMutableTreeNode child;
-				for (Patient p : myPatients) {
-					child = new DefaultMutableTreeNode(p.getFirstName()+" "+p.getLastName());
-					root.add(child);
-				}
-				
-				JTree tree = new JTree(root);
-				
-				tree.getSelectionModel().addTreeSelectionListener(new TreeSelectionListener() {
-					
-					public void valueChanged(TreeSelectionEvent e) {
-						JFrame plop = new JFrame();
-						plop.setSize(100, 100);
-						plop.setVisible(true);
-					}
-					
-				});
-				
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode((user.getLastName()+" "+user.getFirstName()).toString());
+
+		ArrayList<Patient> myPatients = null;
+
+		try {
+			myPatients = DB_connector.getInstance().getListPatientFromDoctor((Doctor) user);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		DefaultMutableTreeNode child;
+		for (Patient p : myPatients) {
+			child = new DefaultMutableTreeNode(p.getFirstName()+" "+p.getLastName());
+			root.add(child);
+		}
+
+		JTree tree = new JTree(root);
+
+		tree.getSelectionModel().addTreeSelectionListener(new TreeSelectionListener() {
+
+			public void valueChanged(TreeSelectionEvent e) {
+				JFrame plop = new JFrame();
+				plop.setSize(100, 100);
+				plop.setVisible(true);
+			}
+
+		});
+
 		this.add(tree);
 	}
 }
