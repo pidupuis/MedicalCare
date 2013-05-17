0ef6ef6a0e86640af3f92914110018c26e53a624
pidupuis <pmj.dupuis@gmail.com>
2013-05-17 12:16:28 +0200

    work on tree panel

---
+++ MedicalCare/src/main/java/ui/dailysheetfilling/PatientNode.java
@@ -0,0 +1,20 @@
+package ui.dailysheetfilling;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import persons.Patient;
+
+public class PatientNode extends DefaultMutableTreeNode {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Patient myPatient;
+	
+	public PatientNode(Patient myPatient) {
+		this.myPatient = myPatient;
+	}
+	
+	public Patient getMyPatient() {
+		return myPatient;
+	}
+}
