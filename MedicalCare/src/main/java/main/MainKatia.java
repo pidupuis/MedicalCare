77053594691c57743b3258ec014b30a46897916e
kchiron <katia.chiron@gmail.com>
2013-05-21 09:53:13 +0200

    Correction du bug dans les packages de tests

--- MedicalCare/src/main/java/main/MainKatia.java
+++ MedicalCare/src/main/java/main/MainKatia.java
@@ -68,11 +68,13 @@ public class MainKatia {
         Doctor dct = db.getDoctorById("21");
         DailyTest d = db.getDailyTests("1", "05");
         //System.out.println("D1 : " + d.getObservations());
-        BloodTest bt = new BloodTest(23.5f, 23.5f, 23.5f, 23.5f, 23.5f, p);
+        //BloodTest bt = new BloodTest(23.5f, 23.5f, 23.5f, 23.5f, 23.5f, p);
         EEG eeg = new EEG(8);
         EffortTest et = new EffortTest(70, 180, 100);
         //db.addDailyTest(d, p, dct, eeg, bt, et);
-        db.getLot("1565");
+        Lot l = db.getLotByIdPatient("1565");
+        System.out.println("numlot : " + l.getNumero());
+        l = db.getLotByIdFiche("1");
         
     }
 
