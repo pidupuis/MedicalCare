5c599b6420ecd2235de98246dfab8b5c105d94fe
Mathieu <Mathieu@172.16.124.39>
2013-05-21 09:52:28 +0200

    minor changes 2

--- MedicalCare/src/main/java/ui/dailysheetfilling/listeners/ListenerPresDias.java
+++ MedicalCare/src/main/java/ui/dailysheetfilling/listeners/ListenerPresDias.java
@@ -1,5 +1,78 @@
 package ui.dailysheetfilling.listeners;
 
-public class ListenerPresDias {
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+import ui.dailysheetfilling.FormPanel;
+
+public class ListenerPresDias implements FocusListener, ActionListener {
+
+	
+	private FormPanel fp;
+	
+	public ListenerPresDias(FormPanel fp) {
+		this.fp = fp;
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		String content = fp.getPres_dias_txf().getText();
+		
+		int cpt = content.length();
+		
+		if (cpt < 4)
+		{
+						
+			try {
+				int errorTry = Integer.valueOf(content);
+				
+				System.out.println(errorTry);
+				
+				if (errorTry<30 || errorTry>300){
+					throw new Exception();
+				}
+				else if (errorTry<60 || errorTry > 140)
+				{
+					fp.getPres_sys_txf().setBackground(Color.ORANGE);
+					fp.getWarning_lbl().setVisible(false);
+					fp.getSuivant_btn().setEnabled(true);
+				}
+				else {
+					fp.getPres_sys_txf().setBackground(Color.WHITE);
+					fp.getWarning_lbl().setVisible(false);
+					fp.getSuivant_btn().setEnabled(true);
+				}
+			} catch (Exception e1) {
+				fp.setCorrect(1, false);
+				fp.getPres_sys_txf().setBackground(Color.RED);
+				fp.getWarning_lbl().setVisible(true);
+				fp.getWarning_lbl().setErrorMessage("Cette saisie contient des lettres ou caractères interdits");
+				fp.getSuivant_btn().setEnabled(false);			
+			}
+		}
+		else
+		{
+			fp.setCorrect(1, false);
+			fp.getPres_sys_txf().setBackground(Color.RED);
+			fp.getWarning_lbl().setVisible(true);
+			fp.getWarning_lbl().setErrorMessage("La valeur de pression systolique est invalide");
+			fp.getSuivant_btn().setEnabled(false);
+		}
+		
+	}	
+
+
+	public void focusGained(FocusEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void focusLost(FocusEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
