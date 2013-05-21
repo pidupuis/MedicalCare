f5e9c1300611ad77a3087e32386ca97d9854a424
Piotr <pierre.cressant@gmail.com>
2013-05-21 10:36:47 +0200

    Changed all BloodTest

--- MedicalCare/src/main/java/tests/BloodTest.java
+++ MedicalCare/src/main/java/tests/BloodTest.java
@@ -7,33 +7,31 @@ import persons.*;
 
 public class BloodTest extends Analysis {
 
-
-    
     private float Hb;
     private float GR;
     private float GB;
     private float hemato;
     private float P;
+    private Patient pat;
 
-    public BloodTest(float Hb, float GR, float GB, float hemato, float P, Patient p) throws Exception {
+    public BloodTest(float Hb, float GR, float GB, float hemato, float P, Patient pat) throws Exception {
         this.Hb = Hb;
         this.GR = GR;
         this.GB = GB;
         this.hemato = hemato;
         this.P = P;
-    
+        this.pat = pat;
+
         try {
-            checkResult(p);
+            checkResult();
         } catch (Exception e) {
-            throw new Exception ("The results is not good!");
+            throw new Exception("The results is not good!");
         }
 
     }
 
-    
-   
-
-    public BloodTest() {}
+    public BloodTest() {
+    }
 
     /**
      *
@@ -45,13 +43,13 @@ public class BloodTest extends Analysis {
      * @param p
      * @throws Exception
      */
-    public void checkResult(Patient p) throws Exception {
+    public void checkResult() throws Exception {
         if (P < 150.0 || P > 400.0) {
             throw new Exception("the result given was not included in the physiologic norms");
         }
         if (GB < 4.0 || GB > 10.0) {
             throw new Exception("the result given was not included in the physiologic norms");
-        } else if (p.getGender() == false) {
+        } else if (this.pat.getGender() == false) {
 
             if (Hb < 12.5 || Hb > 15.5) {
                 throw new Exception("the result given was not included in the physiologic norms");
@@ -88,13 +86,46 @@ public class BloodTest extends Analysis {
      * @param p
      * @throws Exception
      */
-    public boolean checkAllResults(Patient p) throws Exception {
+    public boolean checkP() {
+        return !(P < 150.0 || P > 400.0);
+
+    }
+
+    public boolean checkGB() {
+        return !(GB < 4.0 || GB > 10.0);
+    }
+
+    public boolean checkHb() {
+        if (this.pat.getGender()) {
+            return !(Hb < 14.0 || Hb > 17.0);
+        }
+
+        return !(Hb < 12.5 || Hb > 15.5);
+    }
+
+    public boolean checkGR() {
+        if (this.pat.getGender()) {
+            return !(GR < 4.4 || GR > 6.0);
+        }
+
+        return !(GR < 3.8 || GR > 5.2);
+    }
+
+    public boolean checkHemato() {
+        if (this.pat.getGender()) {
+            return !(hemato < 40.0 || hemato > 52.0);
+        }
+
+        return !(hemato < 37.0 || hemato > 46.0);
+    }
+
+    public boolean checkAllResults() throws Exception {
         if (P < 150.0 || P > 400.0) {
             return false;
         }
         if (GB < 4.0 || GB > 10.0) {
             return false;
-        } else if (p.getGender() == false) {
+        } else if (this.pat.getGender() == false) {
 
             if (Hb < 12.5 || Hb > 15.5) {
                 return false;
@@ -122,13 +153,14 @@ public class BloodTest extends Analysis {
         return true;
     }
 
-    
-
     public float getHb() {
         return Hb;
     }
 
-    public void setHb(float Hb) {
+    public void setHb(float Hb) throws Exception {
+        if (!this.checkHb()) {
+            throw new Exception("Hb value is not good");
+        }
         this.Hb = Hb;
     }
 
@@ -136,7 +168,10 @@ public class BloodTest extends Analysis {
         return GR;
     }
 
-    public void setGR(float GR) {
+    public void setGR(float GR) throws Exception {
+        if (!this.checkGR()) {
+            throw new Exception("GR value is not good");
+        }
         this.GR = GR;
     }
 
@@ -144,7 +179,10 @@ public class BloodTest extends Analysis {
         return GB;
     }
 
-    public void setGB(float GB) {
+    public void setGB(float GB) throws Exception {
+        if (!this.checkGB()) {
+            throw new Exception("GB value is not good");
+        }
         this.GB = GB;
     }
 
@@ -152,7 +190,10 @@ public class BloodTest extends Analysis {
         return hemato;
     }
 
-    public void setHemato(float hemato) {
+    public void setHemato(float hemato) throws Exception {
+        if (!this.checkHemato()) {
+            throw new Exception("Hemato value is not good");
+        }
         this.hemato = hemato;
     }
 
@@ -160,9 +201,23 @@ public class BloodTest extends Analysis {
         return P;
     }
 
-    public void setP(float P) {
+    public void setP(float P) throws Exception {
+        if (!this.checkP()) {
+            throw new Exception("P value is not good");
+        }
         this.P = P;
     }
 
+    public Patient getPat() {
+        return pat;
+    }
 
+    public void setPat(Patient pat) {
+        this.pat = pat;
+    }
+
+    @Override
+    public String toString() {
+        return "BloodTest{" + "Hb=" + Hb + ", GR=" + GR + ", GB=" + GB + ", hemato=" + hemato + ", P=" + P + '}';
+    }
 }
